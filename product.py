6516bf026a20e8850537d5e883125c5b72e7de04
Darshanpatil16 <darshan16062006@gmail.com>
2025-12-01 17:07:39 +0530

    Update product.py

--- product.py
+++ product.py
@@ -3,7 +3,7 @@ def product_info(product_id, name, quantity, price):
         f"Product ID: {product_id}\n"
         f"Name: {name}\n"
         f"Quantity: {quantity}\n"
-        f"Price: {price}"
+        f"Price: {price:.2f}"
     )
 
 if __name__ == "__main__":
